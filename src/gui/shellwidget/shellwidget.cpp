eb28ab926692dc8dcdbd83e3f4ecf533437c36b0
Rui Abreu Ferreira <raf-ep@gmx.com>
2016-08-19 15:08:16 +0100

    Adjust font drawing to account for bearing

--- src/gui/shellwidget/shellwidget.cpp
+++ src/gui/shellwidget/shellwidget.cpp
@@ -153,6 +153,19 @@ void ShellWidget::paintEvent(QPaintEvent *ev)
 
 					// Draw chars at the baseline
 					QPoint pos(r.left(), r.top()+m_ascent+m_lineSpace);
+
+					// Adjust according to font bearing
+					int rBearing = p.fontMetrics().rightBearing(cell.c);
+					int lBearing = p.fontMetrics().leftBearing(cell.c);
+					if (lBearing + rBearing < 0) {
+						// Not much we can do, the font does not
+						// fit the cell
+						qDebug() << p.font() << cell.c << lBearing << rBearing;
+					} else if (lBearing < 0) {
+						pos.setX(pos.x()-lBearing);
+					} else if (rBearing < 0 ) {
+						pos.setX(pos.x()+rBearing);
+					}
 					p.drawText(pos, QString(cell.c));
 				}
 
